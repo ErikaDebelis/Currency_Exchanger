a49e95852113aa38bd4f81c03d4bd39bf25f7a17
Erika Debelis <Erika.Debelis@gmail.com>
2021-07-02 08:36:18 -0700

    FILL in README; rename js file; touch .env + add to .gitignore

--- src/main.js
+++ src/main.js
@@ -2,11 +2,4 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Journal from './journal.js';
-
-
-  $('#').submit(function(event) {
-    event.preventDefault();
-
-  });
-});
+import  from './js/exchange.js';
