115ad1ffc9fdbcda20ee93f6ffb67a6d6e9180d1
Erika Debelis <Erika.Debelis@gmail.com>
2021-07-02 16:18:30 -0700

    WIP:Unsure how to proceed; API call must function to debug properly

--- src/js/exchange.js
+++ src/js/exchange.js
@@ -1,8 +1,7 @@
 export default class Exchanger {
   static async getExchangeRate(dollarAmt, currency) {
     try {      
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/42043223edc19acc44563978/pair/USD/${currency}/${dollarAmt}`);
-      console.log(response);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/q=${currency}/r=${dollarAmt}`);
       if (!response.ok) {
         throw Error(response.statusTextError);
       }
--- src/main.js
+++ src/main.js
@@ -6,11 +6,9 @@ import Exchanger from './js/exchange.js';
 
 function getElements(response) {
   if (response["result"] ==="error") {
-    console.log(response.result);
     $('.showErrors').text(`There was an error: ${response}`);
   } else if (response.result === "success") {
     $('#showRate').text(`parseInt(response["conversion_result"])`);
-    console.log(getElements);  
   } else {
     $('.showErrors').text(`There was an error: ${response["error-type"]}`);
   }
@@ -18,15 +16,12 @@ function getElements(response) {
 async function makeApiCall(dollarAmt) {
   try{
     const response = await Exchanger.getExchangeRate(dollarAmt);
-    console.log(makeApiCall, response["conversion_result"]);
     getElements(response["conversion_result"]);
   }
   catch(err) {
     $('.showErrors').text(`There was an error`);
-    console.log("await failed", err);
   }
 }
-//added try and catch to async to catch errors 
 $(document).ready(function() {
   $('#currency-form').submit(function() {
     const currency = $("#currency option:selected").val();
