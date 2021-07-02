80887be9109d2f961cf9217b967ecd81c953c459
Erika Debelis <Erika.Debelis@gmail.com>
2021-07-02 15:59:59 -0700

    try api call w key in URL- fail to load source tho postman is 200 ok

--- src/js/exchange.js
+++ src/js/exchange.js
@@ -1,7 +1,7 @@
 export default class Exchanger {
   static async getExchangeRate(dollarAmt, currency) {
     try {      
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${dollarAmt}`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/42043223edc19acc44563978/pair/USD/${currency}/${dollarAmt}`);
       console.log(response);
       if (!response.ok) {
         throw Error(response.statusTextError);
