2e2671d1ccde6f0b59aab69c475571ef46f9af48
Erika Debelis <erika.debelis@gmail.com>
2021-07-11 19:37:48 -0400

    fix div spelling error

--- src/main.js
+++ src/main.js
@@ -6,35 +6,37 @@ import Exchanger from './js/exchange.js';
 
 function getElements(response) {
   if (response.result) {
-    $('#showRate').text(`response.conversion_result`);
+    const rate = response.conversion_result.toString();
+    $('.showAmt').text(`${rate}`);
   } else if (response["result"] === "error") {
     $('.showErrors').text(`There was an error: ${response.result}`); 
   } else {
     $('.showErrors').text(`There was an error: ${response["error-type"]}`);
   }
 }
-async function makeApiCall(currency, dollarAmt) {
-  try {
-    const response = await Exchanger.getExchangeRate(currency, dollarAmt);
-    getElements(response);
-  }
-  catch (err) {
-    $('.showErrors').text(`There was an error`);
-  }
-}
+// async function makeApiCall(currency, dollarAmt) {
+//   try {
+//     const response = await Exchanger.getExchangeRate(currency, dollarAmt);
+//     getElements(response);
+//   }
+//   catch (err) {
+//     $('.showErrors').text(`There was an error`);
+//   }
+// }
 
 $('#currency-form').submit(function () {
   const currency = $("#currency option:selected").val();
   let dollarAmt = $('#dollarAmt').val();
-  console.log(dollarAmt);
+
   event.preventDefault();
 
   Exchanger.getExchangeRate(currency, dollarAmt)
 
     .then(function (response) {
       getElements(response);
-      $(".showAmt").html(makeApiCall(currency, dollarAmt));
-      $(".showErrors").text(`ERROR`);
+      //$(".showAmt").html(makeApiCall(currency, dollarAmt));
+      //$(".showAmt").html(makeApiCall(currency, dollarAmt))
+      //$(".showErrors").text(`ERROR`);
     });
 
 });
